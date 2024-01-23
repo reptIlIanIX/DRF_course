08daa4f0c0f05740e0c22ada5bae1067d19e6dba
Denis_b <spawnslash@mail.ru>
2024-01-23 19:08:59 +0300

    tests left

--- users/urls.py
+++ users/urls.py
@@ -10,9 +10,9 @@ app_name = UsersConfig.name
 urlpatterns = [
     path('', UserListAPIView.as_view(), name='users'),
     path('register/', UsersRegistrationView.as_view(), name='register'),
-    path('user/<int:pk>/', UserRetrieveAPIView.as_view(), name='detail'),
-    path('user/update/<int:pk>/', UserUpdateAPIView.as_view(), name='update'),
-    path('user/delete/<int:pk>/', UserDestroyAPIView.as_view(), name='delete'),
+    path('<int:pk>/', UserRetrieveAPIView.as_view(), name='detail'),
+    path('update/<int:pk>/', UserUpdateAPIView.as_view(), name='update'),
+    path('delete/<int:pk>/', UserDestroyAPIView.as_view(), name='delete'),
 
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
